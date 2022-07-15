8c6bbef6e3df1372fb306e5c28c0d7361f7e0848
Chris Vander Linden <vandch02@luther.edu>
2022-07-15 11:50:11 -0500

    enum practice

--- build/module01.js
+++ build/module01.js
@@ -3,3 +3,14 @@ function addNumbers(x, y) {
     return x + y;
 }
 console.log(addNumbers(3, 6));
+let x; //* Explicitly declares x as a number type
+let y = 1; //* Implicitly declares y as a number type
+let z; //* Declares z without initializing it
+var ContractStatus;
+(function (ContractStatus) {
+    ContractStatus[ContractStatus["Permanent"] = 0] = "Permanent";
+    ContractStatus[ContractStatus["Temp"] = 1] = "Temp";
+    ContractStatus[ContractStatus["Apprentice"] = 2] = "Apprentice";
+})(ContractStatus || (ContractStatus = {}));
+let employeeStatus = ContractStatus.Temp;
+console.log(employeeStatus);
